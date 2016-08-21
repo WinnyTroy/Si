e2310143f2fae1fe451b2eb32f1987767d8de88d
WinnyTroy <kiraguwinnie@gmail.com>
2016-08-21 16:39:16 +0300

    first complete template inheritance obtained

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 @app.route('/<name>')  # Allows app to get input from the user without having to use request
 #  Tells flask to capture whatever comes after the forward slash as an argument name
 def index(name="Troy"):
-    return "Hey there, Its {} here".format(name)
+    context = {'name': name}
+    return render_template('index.html', **context)
 
 
 @app.route('/add/<int:num1>/<int:num2>') # conversion of input into into num datatype within URL
@@ -16,7 +17,7 @@ def index(name="Troy"):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-    return render_template('add.html').format(num1, num2, num1+num2)
+    return render_template('add.html', num1=num1, num2=num2).format(num1, num2, num1+num2)
 # debug helps flask autorestart everytime we make changes to our application.
 
 
